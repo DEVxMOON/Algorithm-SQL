47e1a93e4a8e4978cb7be8867195812b02852935
HR <137713546+DEVxMOON@users.noreply.github.com>
2024-04-29 09:50:36 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        
+        if(num1==num2){
+            answer = 1
+        }else{
+            answer =-1;
+        }
+        
+        return answer
+    }
+}
