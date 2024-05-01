871ccad2a558b879fc2afbffda872b403b42622f
HR <137713546+DEVxMOON@users.noreply.github.com>
2024-05-01 09:29:16 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(a: IntArray, b: IntArray): Int {
+        var answer: Int = 0
+        
+        for(i in 0 until a.size){
+            answer += (a[i]*b[i])
+        }
+        
+        return answer
+    }
+}
